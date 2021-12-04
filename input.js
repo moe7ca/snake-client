666963416233f28085c9de624e3426198ff231b1
Ahmed Noori <moe7ca@gmail.com>
2021-12-04 00:08:37 -0500

    Create input.js

---
+++ input.js
@@ -0,0 +1,37 @@
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
+  } else if (key === 'z') {
+    connection.write('Say: Ssssssssssssssss!');
+  } else if (key === 'cx') {
+    connection.write('Say: Hissssssterical!');
+  } else if (key === 'c') {
+    connection.write('Say: I am not a regular snake --- I am a python');
+  }else if (key === 'v') {
+    connection.write('Say: would you like an apple!');
+  };
+};
+
+module.exports = { setupInput };
