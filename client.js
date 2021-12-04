fbde474e49041d228e292e1b122252565542c3a8
Ahmed Noori <moe7ca@gmail.com>
2021-12-04 00:07:31 -0500

    Create client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+const { IP, PORT, NAME } = require('./constants');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write(`Name: ${NAME}`);
+  });
+
+  conn.on('data', data => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = { connect };
